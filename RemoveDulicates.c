e7e13914ede1b30c0dc3934a30e9c341cd65f5f5
Neves <rafaelnvss002@gmail.com>
2025-01-22 11:38:15 -0300

    feat: RemoveDuplicates.c

---
+++ RemoveDulicates.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int removeDuplicates(int* nums, int numsSize) {
+    int j = 1;
+
+    for (int i = 1; i < numsSize; i++) 
+    {
+        if(nums[i] != nums[i - 1]){
+                nums[j] = nums[i];
+                j++;
+            }
+    }    
+    return j;
+}
+
+int main() {
+    int array[] = {0, 0, 0, 1, 2};
+    printf("%d", removeDuplicates(array, 5));
+
+}
